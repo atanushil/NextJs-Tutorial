d45c3527202364aaba64a13fdf501f4fa84051cb
Web Dev Atanu <atanushil358@gmail.com>
2024-10-26 01:54:42 +0530

    Navigation - UI navigation using Link component - href, replace

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 const Home = () => {
   return (
     <div>
         <h1>Welcome Home</h1>
+        <Link href={"/blog"}>Blog</Link>
+        <Link href={"/about"}>About</Link>
+        <Link href={"/products"}>Product</Link>
     </div>
   )
 }
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link"
+
 export default function Product(){
+    const productId=12;
     return(
         <>
-        <h1>Product 1</h1>
-        <h1>Product 2</h1>
-        <h1>Product 3</h1>
-        <h1>Product 4</h1>
+        <Link href={"/"}>Home</Link>
+        <h1><Link href={"/products/1"}>Product 1</Link></h1>
+        <h1><Link href={"/products/2"}>Product 2</Link></h1>
+        <h1><Link href={"/products/3"} replace>Product 3</Link></h1>
+        <h1><Link href={`/products/${productId}`} replace>Product {productId}</Link></h1>
+
         </>
     )
 }
