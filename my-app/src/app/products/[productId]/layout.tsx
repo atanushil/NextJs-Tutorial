34f5b10b88303d880ad27e18f553d97895e37e70
Web Dev Atanu <atanushil358@gmail.com>
2024-10-25 21:46:51 +0530

    nested layout

---
+++ my-app/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default  function ProductDetilsLayout({children}:{children:React.ReactNode}){
+    return (
+        <>
+        {children}
+        <h1>from ProductDetilsLayout</h1>
+        </>
+        )
+}
