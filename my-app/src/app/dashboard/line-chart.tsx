c34923e9645d07dd66e3ad48ff858d61b523fb13
Web Dev Atanu <atanushil358@gmail.com>
2024-10-25 19:27:24 +0530

    file coloction

---
+++ my-app/src/app/dashboard/line-chart.tsx
@@ -0,0 +1,9 @@
+
+
+export default function LineChart() {
+  return (
+    <div>
+      from Line chart
+    </div>
+  )
+}
---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+function BarChart(){
+    return (
+        <div>
+            <h1>Bar Chart</h1>
+        </div>
+    )
+}
+
+export default function Dashboard() {
+  return (
+    <h1>
+      from Dashboard
+    </h1>
+  )
+}
