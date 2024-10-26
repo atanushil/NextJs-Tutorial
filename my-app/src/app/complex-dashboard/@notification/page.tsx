27384295dd6b9bd73470c0393fa55838847e12eb
Web Dev Atanu <atanushil358@gmail.com>
2024-10-27 02:12:54 +0530

    Conditional routes

--- my-app/src/app/complex-dashboard/@notification/page.tsx
+++ my-app/src/app/complex-dashboard/@notification/page.tsx
@@ -2,6 +2,8 @@ import Card from "@/components/Card";
 import Link from "next/link";
 
 export default function Notification(){
+    // console.log("here");
+
     return <Card>
         <div>Notification</div>
         <Link href={"/complex-dashboard/archived"}>Archived</Link>
--- my-app/src/app/complex-dashboard/layout.tsx
+++ my-app/src/app/complex-dashboard/layout.tsx
@@ -6,25 +6,29 @@ export default function Dashboardlayout ( {
     users,
     revenue,
     notification,
+    login,
 }: {
     children: React.ReactNode;
     users: React.ReactNode;
     revenue: React.ReactNode;
     notification: React.ReactNode;
+    login: React.ReactNode;
 } )
 {
-    return (
+    const isLoggedIn = false; // conditional routes
+    return ( isLoggedIn ? (
         <>
-        <div>
-            <div>{children}</div>
-            <div style={{display:"flex"}}>
-                <div style={{display:"flex",flexDirection:"column"}}>
-                    <div>{users}</div>
-                    <div>{revenue}</div>
+            <div>
+                <div>{ children }</div>
+                <div style={ { display: "flex" } }>
+                    <div style={ { display: "flex", flexDirection: "column" } }>
+                        <div>{ users }</div>
+                        <div>{ revenue }</div>
+                    </div>
+                    <div style={ { display: "flex", flex: 1 } }>{ notification }</div>
                 </div>
-                <div style={{display:"flex",flex:1}}>{notification}</div>
             </div>
-        </div>
         </>
+    ) : <div>{ login }</div>
     );
 }
