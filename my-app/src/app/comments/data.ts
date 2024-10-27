bb9a6ed64ade832566dbc350d7e5362d93e66900
Web Dev Atanu <atanushil358@gmail.com>
2024-10-27 16:50:43 +0530

    GET request using route handler

---
+++ my-app/src/app/comments/data.ts
@@ -0,0 +1,5 @@
+export const comments=[
+    {id:1, name:"John", comment:"This is a comment"},
+    {id:2, name:"Jane", comment:"This is another comment"},
+    {id:3, name:"Bob", comment:"This is yet another comment"}
+]
