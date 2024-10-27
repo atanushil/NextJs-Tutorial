40c6d4b3d65bcc1bbe1f2dfadf90b586e0ef6fc5
Web Dev Atanu <atanushil358@gmail.com>
2024-10-27 19:52:35 +0530

    Handling PATCH Request - Update

--- my-app/src/app/comments/[id]/route.ts
+++ my-app/src/app/comments/[id]/route.ts
@@ -8,4 +8,16 @@ export async function GET(
   const comment = comments.find((comment) => comment.id === parseInt(id));
   return Response.json(comment);
 }
- 
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json();
+  const { comment } = body;
+  const { id } = params;
+  const commentIndex = comments.findIndex(
+    (comment) => comment.id === parseInt(id)
+  );
+  comments[commentIndex].comment = comment;
+  return Response.json(comments[commentIndex]);
+}
