5eb9d51e0ddf7dd9393fc0ca24e0ece35d84f6b1
Web Dev Atanu <atanushil358@gmail.com>
2024-10-27 16:35:12 +0530

    Route handler

---
+++ my-app/src/app/dashboard/api/route.ts
@@ -0,0 +1,5 @@
+export async function GET() {
+    return new Response("Dashboard route")
+}
+
+// for working to of the route we have to make a new folder for api ,there you have to put the  route
---
+++ my-app/src/app/route-handler/route.ts
@@ -0,0 +1,3 @@
+export async function GET() {
+  return new Response("Hello, this is my first code to use  route handler ");
+}
